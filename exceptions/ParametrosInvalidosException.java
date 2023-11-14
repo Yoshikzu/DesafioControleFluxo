9f89c7e0f6027ba97dafa9676657e9834600f6ca
yoshikzu <marcelo_yoshikazu@hotmail.com>
2023-11-13 21:08:22 -0300

    Criação da Classe Contador e Implementação do Desafio Controle Fluxo

---
+++ exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class ParametrosInvalidosException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public ParametrosInvalidosException(){
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
